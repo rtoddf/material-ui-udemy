7f10772113c4e1bb0eec2ec89f017a07ea097529
rtoddf <rtoddf@me.com>
2020-04-17 18:24:37 -0400

    finishing setup

---
+++ src/Components/App.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <h1>hello</h1>
+        )
+    }
+}
+
+export default App;
