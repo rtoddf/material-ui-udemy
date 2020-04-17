00350c96f4ec3fb78b79e5ac13f3dccec77338c3
rtoddf <rtoddf@me.com>
2020-04-17 18:47:19 -0400

    header start

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,20 @@
 import React, { Fragment } from 'react';
+import Header from "./UI/Header";
 
 class App extends React.Component {
     render() {
         return (
-            <h1>hello</h1>
+            <Fragment>
+                <Header />
+                {[...new Array(120)]
+                    .map(
+                    () => `Cras mattis consectetur purus sit amet fermentum.
+        Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+        Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+        Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
+                    )
+                    .join('\n')}
+            </Fragment>
         )
     }
 }
---
+++ src/Components/UI/Header.js
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import { AppBar, Toolbar, Typography, Button, IconButton } from "@material-ui/core";
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+
+function ElevationScroll(props) {
+    const { children } = props;
+
+    const trigger = useScrollTrigger({
+        disableHysteresis: true,
+        threshold: 0
+    });
+
+    return React.cloneElement(children, {
+        elevation: trigger ? 4 : 0,
+    });
+}
+
+
+export default function Header(props) {
+    return (
+        <ElevationScroll>
+            <AppBar>
+                <Toolbar>
+                    Arc Development
+                </Toolbar>
+            </AppBar>
+        </ElevationScroll>
+    )
+}
+
+ElevationScroll.propTypes = {
+    children: PropTypes.element.isRequired,
+    /**
+     * Injected by the documentation to work in an iframe.
+     * You won't need it on your project.
+     */
+    window: PropTypes.func,
+    };  
+    
